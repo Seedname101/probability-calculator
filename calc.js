86ac93f89fc8e9992b0b2ebe948ae51432a01712
Seedname101 <45999377+Seedname101@users.noreply.github.com>
2021-08-13 17:20:25 -0400

    Update calc.js

--- calc.js
+++ calc.js
@@ -1,48 +1,67 @@
+document.getElementById("people").style.display = "none";
+var sim = false;
+function peopleToggle(){
+  var x = document.getElementById("people");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+    sim = true;
+  } else {
+    x.style.display = "none";
+    sim = false;
+  }
+}
+
 function random(min, max) {
 	return Math.floor((Math.random() * (max - min + 1)) + min);
 }
 
-let chance = 0.0041/20;
-let opens = 10000;
-let people = 10000;
 
-let numbersPulled = [];
-let amtOfNums = Math.floor(1 / chance);
-let target = Math.floor(random(1, amtOfNums));
-let totalHit = 0;
-let timesHit = 0;
-let achieved = false;
+function getData(){
+  let chance = document.querySelectorAll('input')[0].value;
+  let opens = document.querySelectorAll('input')[1].value;
+  let people = document.querySelectorAll('input')[3].value;
+  
+  let numbersPulled = [];
+	let amtOfNums = Math.floor(1 / chance);
+	let target = Math.floor(random(1, amtOfNums));
+	let totalHit = 0;
+	let timesHit = 0;
+	let achieved = false;
 
-let simulation = true;
+	let mathChance = (1 - Math.pow((1 - chance), opens))*100;
 
-let mathChance = (1 - Math.pow((1 - chance), opens))*100;
+  if(sim){
+      for(let person = 0; person < people; person ++){
 
-if(simulation){
-    for(let person = 0; person < people; person ++){
-        
-        for(let i = 0; i < opens; i ++){
-            numbersPulled.push(Math.floor(random(1, amtOfNums)));
-        }
-        
-        
-        for(let i = 0; i < amtOfNums; i++){
-            if(numbersPulled[i] == target){
-                timesHit ++;
-                achieved = true;
-            }
-        }
-        
-        if(achieved){
-            totalHit ++;
-        }
-        
-        numbersPulled = [];
-        achieved = false;
-        
-    }
+          for(let i = 0; i < opens; i ++){
+              numbersPulled.push(Math.floor(random(1, amtOfNums)));
+          }
+
+
+          for(let i = 0; i < amtOfNums; i++){
+              if(numbersPulled[i] == target){
+                  timesHit ++;
+                  achieved = true;
+              }
+          }
+
+          if(achieved){
+              totalHit ++;
+          }
+
+          numbersPulled = [];
+          achieved = false;
+
+      }
+  } 
+
+  let simChance = 100*(totalHit/people);
+  document.getElementById('math').innerHTML = "Mathematical Probability: " + mathChance.toFixed(3) + "%";
+  
+  if(sim){
+    document.getElementById('sim').innerHTML = "Simulated Probability: " + simChance.toFixed(3) + "%";
+    document.getElementById('total').innerHTML = "Total Times  Hit: " + timesHit;
+  }
 }
 
-let simChance = 100*(totalHit/people);
-document.getElementById('math').innerHTML = "Mathematical Probability: " + mathChance.toFixed(3) + "%";
-document.getElementById('sim').innerHTML = "Simulated Probability: " + simChance.toFixed(3) + "%";
-document.getElementById('total').innerHTML = "Total Times  Hit: " + timesHit;
+
